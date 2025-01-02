5e954a5988d1eabe737713a9ccb83ae955e55525
Helder Vasconcelos <heldervasc@bearstouch.com>
2025-01-02 15:02:29 +0000

    Docusaurus

---
+++ docs/intro.md
@@ -0,0 +1,30 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# BakerFi Documentation
+
+***Unlocking Superior Yields through Diversified & Curated DeFi Strategies***
+
+## Introduction
+
+BakerFi is a decentralized DeFi protocol where users can exponentiate their APY by depositing ETH. Then, the protocol automatically implements recursive strategies to achieve higher gains.
+
+The recursive part in this case refers to having a leveraged position using existing lending protocols, that ensure that you're able to get a higher yield with a minimized risk.
+
+## Current Problem
+
+Staking tokens has evolved into a prominent mechanism for users to generate returns. However, the challenge of maximizing Annual Percentage Yields (APYs) through recursive staking and interactions with lending pools remains daunting and cumbersome. Furthermore, a significant portion of users, lacking a comprehensive understanding of decentralized finance (DeFi), are unable to fully capitalize on available opportunities.
+
+## Solution
+
+Introducing BakerFi, a pioneering DeFi solution that employs Recursive Liquid Staking Strategies to automate and optimize APYs within the Ethereum L2 ecosystem, starting with Base and Arbitrum. Our smart contract automates the recursive staking process, enabling users to exponentially grow their assets with a single interaction. Not having to be continuously active and manually optimizing it, users simply select their preferred ETH amount and initiate the staking process by clicking the “Deposit” button. From there, our automated smart contract takes over, executing all necessary actions to optimize their returns.
+
+[Getting Started](https://www.notion.so/Getting-Started-554e4d953fe24d68987020ac67c3a3e6?pvs=21)
+
+[Protocol](https://www.notion.so/Protocol-fdcf373523424aa1bdddf95dec3381f7?pvs=21)
+
+[Governance](https://www.notion.so/Governance-54939489856c4fe7b38ededadce4ee33?pvs=21)
+
+[BakerFi Points Program](https://www.notion.so/BakerFi-Points-Program-89c4eeb232e948c8b8eb95b286b09bbb?pvs=21)
---
+++ docusaurus.config.ts
@@ -0,0 +1,123 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Bakerfi Docs',
+  tagline: 'Bakerfi Docs',
+  favicon: 'img/favicon.ico',
+  url: 'https://docs.bakerfi.com',
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'baker-fi', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Bakerfi Docs',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: 'https://blog.bakerfi.com', label: 'Bakerfi Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Bakerfi Landing Page',
+              href: 'https://bakerfi.com',
+            },
+            {
+              label: 'Bakerfi App',
+              href: 'https://bakerfi.com/app',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://x.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} BakerFi, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+
+};
+
+export default config;
